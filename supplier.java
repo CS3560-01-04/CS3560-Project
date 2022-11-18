e8bed5b07409e542e712e2857b2011bc8550281d
Bvlvq <89806814+Bvlvq@users.noreply.github.com>
2022-11-17 20:41:18 -0800

    supplier class

---
+++ supplier.java
@@ -0,0 +1,66 @@
+public class Supplier {
+
+    private int SupplierID;
+    private String SupplierName;
+    private String merchandise;
+    private int Quantity;
+    private int Price;
+
+    public Drug() {
+        this.SupplierID = 0;
+        this.SupplierName = ""; 
+        this.merchandise = "";
+        this.Quantity = 0;
+        this.Price = 0;
+    }
+
+    public Drug(int SupplierID, String SupplierName, String merchandise, int Quantity, 
+            int Price) {
+        this.SupplierID = SupplierID;
+        this.SupplierName = SupplierName;
+        this.merchandise = merchandise;
+        this.Quantity = Quantity;
+        this.Price = Price;
+    }
+
+    public int getSupplierID() {
+        return SupplierID;
+    }
+
+    public void setSupplierID(int supplierID) {
+        SupplierID = supplierID;
+    }
+
+    public String getSupplierName() {
+        return SupplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        SupplierName = supplierName;
+    }
+
+    public String getMerchandise() {
+        return merchandise;
+    }
+
+    public void setMerchandise(String merchandise) {
+        this.merchandise = merchandise;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+}
