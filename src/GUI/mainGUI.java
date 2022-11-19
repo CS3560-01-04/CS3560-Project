846721415cd82780310acfed6c9625872037d08f
JustAManji <89673062+JustAManji@users.noreply.github.com>
2022-11-19 06:52:45 -0800

    updated gui to include supplier class
    includes the current supplier class and changed the names of a couple values. supplier class should be changed though as some of the attributes don't seem relevant or necessary.

--- src/GUI/mainGUI.java
+++ src/GUI/mainGUI.java
@@ -16,7 +16,8 @@ public class mainGUI extends Application {
 	Stage window;
 	Scene login, homePage;
 	Button submitBtn;
-	TableView<Drug> table;
+	TableView<Drug> drugTable;
+	TableView<Supplier> supplierTable;
 	TextField drugIDInput;
 	TextField drugNameInput;
 	TextField drugDescriptionInput;
@@ -26,6 +27,11 @@ public class mainGUI extends Application {
 	TextField drugExpyearInput;
 	TextField drugExpmonthInput;
 	TextField drugExpdayInput;
+	TextField SupplierIDInput;
+	TextField SupplierNameInput;
+	TextField SupplierMerchandiseInput;
+	TextField SupplierQuantityInput;
+	TextField SupplierPriceInput;
 	
 	//just to launch out GUI
 	public static void main(String[] args) {
@@ -36,8 +42,8 @@ public class mainGUI extends Application {
 	public void start(Stage arg0) throws Exception {
 		
 		//test username and password for the sake of demonstration, will be removed later
-		String testUsrname = "ManjiHui777";
-		String testPasswrd = "GUIisCool251";
+		String testUsrname = "1";
+		String testPasswrd = "1";
 		
 		window = arg0;
 		window.setTitle("Login Screen");
@@ -117,29 +123,29 @@ public class mainGUI extends Application {
 		login = new Scene(loginGrid, 400, 300);
 		
 		//ID column
-		TableColumn<Drug, String> IDColumn = new TableColumn<>("ID");
-		IDColumn.setMinWidth(65);
-		IDColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+		TableColumn<Drug, String> drugIDColumn = new TableColumn<>("ID");
+		drugIDColumn.setMinWidth(65);
+		drugIDColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
 		
 		//Name column
-		TableColumn<Drug, String> nameColumn = new TableColumn<>("Name");
-		nameColumn.setMinWidth(150);
-		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		TableColumn<Drug, String> drugNameColumn = new TableColumn<>("Name");
+		drugNameColumn.setMinWidth(150);
+		drugNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 		
 		//description column
-		TableColumn<Drug, String> descriptionColumn = new TableColumn<>("Description");
-		descriptionColumn.setMinWidth(200);
-		descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+		TableColumn<Drug, String> drugDescriptionColumn = new TableColumn<>("Description");
+		drugDescriptionColumn.setMinWidth(200);
+		drugDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
 		
 		//quantity column
-		TableColumn<Drug, Integer> quantityColumn = new TableColumn<>("Quantity");
-		quantityColumn.setMinWidth(50);
-		quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+		TableColumn<Drug, Integer> drugQuantityColumn = new TableColumn<>("Quantity");
+		drugQuantityColumn.setMinWidth(50);
+		drugQuantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 		
 		//price column
-		TableColumn<Drug, Double> priceColumn = new TableColumn<>("Price");
-		priceColumn.setMinWidth(50);
-		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+		TableColumn<Drug, Double> drugPriceColumn = new TableColumn<>("Price");
+		drugPriceColumn.setMinWidth(50);
+		drugPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 		
 		//supplier column
 		TableColumn<Drug, String> supplierColumn = new TableColumn<>("Supplier");
@@ -160,6 +166,31 @@ public class mainGUI extends Application {
 		TableColumn<Drug, Integer> expdayColumn = new TableColumn<>("Exp day");
 		expdayColumn.setMinWidth(50);
 		expdayColumn.setCellValueFactory(new PropertyValueFactory<>("expday"));
+		
+		//Supplier ID column
+		TableColumn<Supplier, Integer> SupplierIDColumn = new TableColumn<>("Supplier ID");
+		SupplierIDColumn.setMinWidth(65);
+		SupplierIDColumn.setCellValueFactory(new PropertyValueFactory<>("SupplierID"));
+		
+		//Supplier Name column
+		TableColumn<Supplier, String> SupplierNameColumn = new TableColumn<>("Supplier Name");
+		SupplierNameColumn.setMinWidth(200);
+		SupplierNameColumn.setCellValueFactory(new PropertyValueFactory<>("SupplierName"));
+		
+		//Supplier Name column
+		TableColumn<Supplier, String> SupplierMerchandiseColumn = new TableColumn<>("Supplier Merchandise");
+		SupplierMerchandiseColumn.setMinWidth(200);
+		SupplierMerchandiseColumn.setCellValueFactory(new PropertyValueFactory<>("merchandise"));
+		
+		//Supplier Quantity column
+		TableColumn<Supplier, Integer> SupplierQuantityColumn = new TableColumn<>("Supplier Quantity");
+		SupplierQuantityColumn.setMinWidth(100);
+		SupplierQuantityColumn.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
+		
+		//Supplier Price column
+		TableColumn<Supplier, Integer> SupplierPriceColumn = new TableColumn<>("Supplier Price");
+		SupplierPriceColumn.setMinWidth(100);
+		SupplierPriceColumn.setCellValueFactory(new PropertyValueFactory<>("Price"));
 		
 		//ID Input
 		drugIDInput = new TextField();
@@ -206,36 +237,103 @@ public class mainGUI extends Application {
 		drugExpdayInput.setPromptText("Exp day of drug");
 		drugExpdayInput.setMaxWidth(200);
 		
+		//Supplier ID Input
+		SupplierIDInput = new TextField();
+		SupplierIDInput.setPromptText("ID of Supplier");
+		SupplierIDInput.setMaxWidth(200);
+		
+		//Supplier Name Input
+		SupplierNameInput = new TextField();
+		SupplierNameInput.setPromptText("Name of Supplier");
+		SupplierNameInput.setMaxWidth(200);
+		
+		//Supplier Name Input
+		SupplierMerchandiseInput = new TextField();
+		SupplierMerchandiseInput.setPromptText("Merchandise of Supplier");
+		SupplierMerchandiseInput.setMaxWidth(200);
+		
+		//Supplier Quantity Input
+		SupplierQuantityInput = new TextField();
+		SupplierQuantityInput.setPromptText("Quantity of Supplier");
+		SupplierQuantityInput.setMaxWidth(200);
+		
+		//Supplier Price Input
+		SupplierPriceInput = new TextField();
+		SupplierPriceInput.setPromptText("Price of Supplier");
+		SupplierPriceInput.setMaxWidth(200);
+		
 		//Buttons to add or delete drugs
-		Button addButton = new Button("Add");
-		addButton.setOnAction(e -> addButtonClicked());
-		Button deleteButton = new Button("Delete");
-		deleteButton.setOnAction(e -> {
+		Button drugAddButton = new Button("Add");
+		drugAddButton.setOnAction(e -> drugAddButtonClicked());
+		Button drugDeleteButton = new Button("Delete");
+		drugDeleteButton.setOnAction(e -> {
 			boolean delete = ConfirmBox.display("Confirm Deletion", "Are you sure you want to delete this product?");
 			if(delete)
-				deleteButtonClicked();
+				drugDeleteButtonClicked();
 		});
 		
-		HBox hBox = new HBox();
-		hBox.setPadding(new Insets(10, 10, 10, 10));
-		hBox.setSpacing(10);
-		hBox.getChildren().addAll(drugIDInput, drugNameInput, drugDescriptionInput, drugPriceInput, drugQuantityInput,
-				drugSupplierInput, drugExpyearInput, drugExpmonthInput, drugExpdayInput, addButton, deleteButton);
+		//Buttons to add or delete suppliers
+		Button supplierAddButton = new Button("Add");
+		supplierAddButton.setOnAction(e -> supplierAddButtonClicked());
+		Button supplierDeleteButton = new Button("Delete");
+		supplierDeleteButton.setOnAction(e -> {
+			boolean delete = ConfirmBox.display("Confirm Deletion", "Are you sure you want to delete this supplier?");
+			if(delete)
+				supplierDeleteClicked();
+		});
 		
-		table = new TableView<>();
-		table.setItems(getProduct());
-		table.getColumns().addAll(IDColumn, nameColumn, descriptionColumn, priceColumn, quantityColumn,
+		Label drugHBoxLabel = new Label("Add or Delete Drugs: ");
+		HBox drugHBox = new HBox();
+		drugHBox.setPadding(new Insets(10, 10, 10, 10));
+		drugHBox.setSpacing(10);
+		drugHBox.getChildren().addAll(drugHBoxLabel, drugIDInput, drugNameInput, drugDescriptionInput, drugPriceInput, drugQuantityInput,
+				drugSupplierInput, drugExpyearInput, drugExpmonthInput, drugExpdayInput, drugAddButton, drugDeleteButton);
+		
+		drugTable = new TableView<>();
+		drugTable.setItems(getDrug());
+		drugTable.getColumns().addAll(drugIDColumn, drugNameColumn, drugDescriptionColumn, drugPriceColumn, drugQuantityColumn,
 				supplierColumn, expyearColumn, expmonthColumn, expdayColumn);
 		
-		VBox vBox = new VBox();
-		vBox.getChildren().addAll(table);
+		Label drugVBoxLabel = new Label("Drug Table");
+		VBox drugVBox = new VBox();
+		drugVBox.setPadding(new Insets(10, 10, 10, 10));
+		drugVBox.setSpacing(10);
+		drugVBox.getChildren().addAll(drugVBoxLabel, drugTable);
+		
+		Label supplierHBoxLabel = new Label("Add or Delete Suppiers: ");
+		HBox supplierHBox = new HBox();
+		supplierHBox.setPadding(new Insets(10, 10, 10, 10));
+		supplierHBox.setSpacing(10);
+		supplierHBox.getChildren().addAll(supplierHBoxLabel, SupplierIDInput, SupplierNameInput, SupplierMerchandiseInput, SupplierPriceInput, SupplierQuantityInput,
+				supplierAddButton, supplierDeleteButton);
+		
+		supplierTable = new TableView<>();
+		supplierTable.setItems(getSupplier());
+		supplierTable.getColumns().addAll(SupplierIDColumn, SupplierNameColumn, SupplierMerchandiseColumn, SupplierPriceColumn, SupplierQuantityColumn);
+		
+		Label supplierVBoxLabel = new Label("Supplier Table");
+		VBox supplierVBox = new VBox();
+		supplierVBox.setPadding(new Insets(10, 10, 10, 10));
+		supplierVBox.setSpacing(10);
+		supplierVBox.getChildren().addAll(supplierVBoxLabel, supplierTable);
+		
+		VBox combinedVBox = new VBox();
+		combinedVBox.setPadding(new Insets(10, 10, 10, 10));
+		combinedVBox.setSpacing(10);
+		combinedVBox.getChildren().addAll(drugVBox, supplierVBox);
+		
+		VBox combinedHBox = new VBox();
+		combinedHBox.setPadding(new Insets(10, 10, 10, 10));
+		combinedHBox.setSpacing(10);
+		combinedHBox.getChildren().addAll(drugHBox, supplierHBox);
+		
 		
 		//Creating a border pane layout for our second scene
 		BorderPane layout2 = new BorderPane();
 		int width = ((int) Screen.getPrimary().getBounds().getWidth()) - 50;
 		int height = ((int) Screen.getPrimary().getBounds().getHeight() - 100);
-		layout2.setCenter(vBox);
-		layout2.setBottom(hBox);
+		layout2.setCenter(combinedVBox);
+		layout2.setBottom(combinedHBox);
 		homePage = new Scene(layout2, width, height);
 		
 		//Setting the login page as the first thing seen when program is run, and showing the window
@@ -244,7 +342,7 @@ public class mainGUI extends Application {
 	}
 	
 	//add button clicked
-	public void addButtonClicked() {
+	public void drugAddButtonClicked() {
 		
 		if((Validator.validation("Integer", drugIDInput.getText())) &&
 		(Validator.validation("Double", drugPriceInput.getText())) &&
@@ -262,7 +360,7 @@ public class mainGUI extends Application {
 			drug.setExpyear(Integer.parseInt(drugExpyearInput.getText()));
 			drug.setExpmonth(Integer.parseInt(drugExpmonthInput.getText()));
 			drug.setExpday(Integer.parseInt(drugExpdayInput.getText()));
-			table.getItems().add(drug);
+			drugTable.getItems().add(drug);
 			drugIDInput.clear();
 			drugNameInput.clear();
 			drugDescriptionInput.clear();
@@ -280,16 +378,51 @@ public class mainGUI extends Application {
 		
 	}
 	
+	//add button clicked
+	public void supplierAddButtonClicked() {
+		
+		if((Validator.validation("Integer", SupplierIDInput.getText())) &&
+		(Validator.validation("Integer", SupplierQuantityInput.getText())) &&
+		(Validator.validation("Integer", SupplierPriceInput.getText()))) {
+			Supplier supplier = new Supplier();
+			supplier.setSupplierID(Integer.parseInt(SupplierIDInput.getText()));
+			supplier.setSupplierName(SupplierNameInput.getText());
+			supplier.setMerchandise(SupplierMerchandiseInput.getText());
+			supplier.setQuantity(Integer.parseInt(SupplierQuantityInput.getText()));
+			supplier.setPrice(Integer.parseInt(SupplierPriceInput.getText()));
+			supplierTable.getItems().add(supplier);
+			SupplierIDInput.clear();
+			SupplierNameInput.clear();
+			SupplierMerchandiseInput.clear();
+			SupplierQuantityInput.clear();
+			SupplierPriceInput.clear();
+		}
+		
+		else {
+			AlertBox.display("Error in input", "Sorry but one of the values you inputed was of the wrong type, please try again.", "Try Again");
+		}
+		
+	}
+	
 	//delete button clicked
-	public void deleteButtonClicked() {
+	public void drugDeleteButtonClicked() {
 		ObservableList<Drug> drugSelected, allDrugs;
-		allDrugs = table.getItems();
-		drugSelected = table.getSelectionModel().getSelectedItems();
+		allDrugs = drugTable.getItems();
+		drugSelected = drugTable.getSelectionModel().getSelectedItems();
 		
 		drugSelected.forEach(allDrugs::remove);
 	}
 	
-	public ObservableList<Drug> getProduct(){
+	//delete button clicked
+	public void supplierDeleteClicked() {
+		ObservableList<Supplier> supplierSelected, allSuppliers;
+		allSuppliers = supplierTable.getItems();
+		supplierSelected = supplierTable.getSelectionModel().getSelectedItems();
+		
+		supplierSelected.forEach(allSuppliers::remove);
+	}
+	
+	public ObservableList<Drug> getDrug(){
 		ObservableList<Drug> drugs = FXCollections.observableArrayList();
 		drugs.add(new Drug(4598, "Albuterol Sulfate", "Bronchospasm treatment; Beta2 Agonists",
 				200, 50.68, "CVS", 2026, 4, 15));
@@ -302,6 +435,21 @@ public class mainGUI extends Application {
 		drugs.add(new Drug(5468, "Tylenol", "over-the-counter pain relief; Analgesics",
 				250, 11.99, "CVS", 2026, 7, 17));
 		return drugs;
+	}
+	
+	public ObservableList<Supplier> getSupplier(){
+		ObservableList<Supplier> suppliers = FXCollections.observableArrayList();
+		suppliers.add(new Supplier(4598, "CVS", "Bronchospasm treatment; Beta2 Agonists",
+				200, 50));
+		suppliers.add(new Supplier(1556, "CVS", "central nervous system stimulant",
+				450, 187));
+		suppliers.add(new Supplier(7855, "CVS", "nonsteroidal anti-inflammatory drug",
+				70, 363));
+		suppliers.add(new Supplier(1453, "CVS", "Antidepressant; major depressive disorder treatment",
+				95, 620));
+		suppliers.add(new Supplier(5468, "CVS", "over-the-counter pain relief; Analgesics",
+				250, 11));
+		return suppliers;
 	}
 	
 	
