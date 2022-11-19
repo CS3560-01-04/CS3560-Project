1a4066b593a4c7a85b1ee6e66ccda3ab0fbf4acf
BrandonLeho <89223038+BrandonLeho@users.noreply.github.com>
2022-11-18 16:31:26 -0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -20,6 +20,9 @@ public class Test
 		try
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			//sql url: jdbc:mysql://localhost:3306/cs3560
+			//user name: root
+			//password: bruh
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs3560", "root", "bruh");
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM product WHERE Description = 'Pain Reliever'");
