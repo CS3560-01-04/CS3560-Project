9e722e06548c005372b6eca10c1e8443cf7fb6b7
BrandonLeho <89223038+BrandonLeho@users.noreply.github.com>
2022-11-18 16:28:50 -0800

    Testing MySQL data reading code
    *MUST HAVE SQL CONNECTOR IN PROGRAM LIBRARY*
    Download connector https://dev.mysql.com/downloads/connector/j/
    Choose "Platform Independent" as the operating system then make sure to download the ZIP file not the TAR
    Once on eclipse (idk how to do it on vscode)
    right click on project folder -> build path -> configure build path -> libraries -> click/highlight modulepath -> Add external JARs -> mysql-connector-j-8.0.31.jar -> apply

---
+++ Test.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Test 
+{
+
+	public static void main(String[] args)
+	{
+		Test pro = new Test();
+		pro.createConenction();
+	}
+	
+	void createConenction()
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs3560", "root", "bruh");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM product WHERE Description = 'Pain Reliever'");
+			while(rs.next())
+			{
+				String name = rs.getString("Description");
+				System.out.println(name);
+			}
+			
+			System.out.println("Database connection success");
+		} 
+		catch (ClassNotFoundException ex)
+		{
+			Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		catch (SQLException ex)
+		{
+			Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
