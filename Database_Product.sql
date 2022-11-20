dbc4e3e29b333923f26909778b23056677b92729
Miguel Sierra <98719795+UndeadBatman@users.noreply.github.com>
2022-11-19 20:45:01 -0800

    Example Database update

--- Database_Product.sql
+++ Database_Product.sql
@@ -9,11 +9,13 @@ CREATE TABLE product_item (
     price DECIMAL (5,2) NOT NULL,
     quantity INT (5),
     supplier VARCHAR(50) NOT NULL,
-    exp_date VARCHAR(10),
+    exp_year VARCHAR(4) NOT NULL,
+    exp_month VARCHAR(2) NOT NULL,
+    exp_day VARCHAR(2) NOT NULL,
     PRIMARY KEY (product_id));
     
-INSERT INTO product_item VALUES(1, 'advil', 'Pain Reliever', 5.99, 70, 'CVS', '2023-06-15');
-INSERT INTO product_item VALUES(2, 'Tylenol', 'Pain Reliever', 3.99, 23, 'CVS', '2023-11-6');
-INSERT INTO product_item VALUES(3, 'Sertrline', 'Increase dopemine levels', 5.99, 14, 'CVS', '2023-05-13');
-INSERT INTO product_item VALUES(4, 'Lactaid', 'Help disgest dairy', 9.99, 44, 'CVS', '2023-04-24');
-INSERT INTO product_item VALUES(5, 'Melatonin', 'Helps sleep more', 7.99, 30, 'CVS', '2023-12-02');
+INSERT INTO product_item VALUES(1, 'advil', 'Pain Reliever', 5.99, 70, 'CVS', '2023', '6', '15');
+INSERT INTO product_item VALUES(2, 'Tylenol', 'Pain Reliever', 3.99, 23, 'CVS', '2023', '11', '6');
+INSERT INTO product_item VALUES(3, 'Sertrline', 'Increase dopemine levels', 5.99, 14, 'CVS', '2023', '5', '13');
+INSERT INTO product_item VALUES(4, 'Lactaid', 'Help disgest dairy', 9.99, 44, 'CVS', '2023', '4', '24');
+INSERT INTO product_item VALUES(5, 'Melatonin', 'Helps sleep more', 7.99, 30, 'CVS', '2023', '12', '2');
