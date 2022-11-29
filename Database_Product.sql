c2a04a3a9b23cdc11b79702eba17cd09a2f1f609
Miguel Sierra <98719795+UndeadBatman@users.noreply.github.com>
2022-11-28 21:49:29 -0800

    Add files via upload

--- Database_Product.sql
+++ Database_Product.sql
@@ -2,21 +2,26 @@ DROP DATABASE IF EXISTS sql_product;
 CREATE DATABASE sql_product;
 USE sql_product;
 
-CREATE TABLE product (
-	row_num INT (10) NOT NULL,
+CREATE TABLE product_item (
 	product_id INT (6) NOT NULL AUTO_INCREMENT,
-    	product_name VARCHAR (50) NOT NULL,
-  	descript VARCHAR (50),
-    	price DECIMAL (5,2) NOT NULL,
-    	quantity INT (5),
-    	supplier VARCHAR(50) NOT NULL,
-    	exp_year VARCHAR(4) NOT NULL,
-    	exp_month VARCHAR(2) NOT NULL,
-    	exp_day VARCHAR(2) NOT NULL,
-    	PRIMARY KEY (product_id));
+    row_num INT (10) NOT NULL,
+    product_name VARCHAR (50) NOT NULL,
+    descript VARCHAR (50),
+    price DECIMAL (5,2) NOT NULL,
+    quantity INT (5),
+    supplier VARCHAR(50) NOT NULL,
+    exp_year VARCHAR(4) NOT NULL,
+    exp_month VARCHAR(2) NOT NULL,
+    exp_day VARCHAR(2) NOT NULL,
+    PRIMARY KEY (product_id));
     
-INSERT INTO product VALUES(1, 2395, 'advil', 'Pain Reliever', 5.99, 70, 'CVS', '2023', '6', '15');
-INSERT INTO product VALUES(2, 9173, 'Tylenol', 'Pain Reliever', 3.99, 23, 'CVS', '2023', '11', '6');
-INSERT INTO product VALUES(3, 0513, 'Sertrline', 'Increase dopemine levels', 5.99, 14, 'CVS', '2023', '5', '13');
-INSERT INTO product VALUES(4, 7239, 'Lactaid', 'Help disgest dairy', 9.99, 44, 'CVS', '2023', '4', '24');
-INSERT INTO product VALUES(5, 6439, 'Melatonin', 'Helps sleep more', 7.99, 30, 'CVS', '2023', '12', '2');
+INSERT INTO product_item VALUES(1, 2395, 'Advil', 'Pain Reliever', 5.99, 70, 'Pfizer', '2023', '6', '15');
+INSERT INTO product_item VALUES(2, 9173, 'Tylenol', 'Pain Reliever', 3.99, 23, 'Johnson & Johnson', '2023', '11', '6');
+INSERT INTO product_item VALUES(3, 0513, 'Sertraline', 'Increase dopemine levels', 5.99, 14, 'Pfizer', '2023', '5', '13');
+INSERT INTO product_item VALUES(4, 7239, 'Lactaid', 'Help disgest dairy', 9.99, 44, 'Johnson & Johnson', '2023', '4', '24');
+INSERT INTO product_item VALUES(5, 6439, 'Melatonin', 'Helps sleep more', 7.99, 30, 'CVS', '2023', '12', '2');
+INSERT INTO product_item VALUES(6, 8566, 'Hormone Blockers', 'Blocking nartural hormone production', 19.99, 50, 'Endo International', '2023', '11', '26');
+INSERT INTO product_item VALUES(7, 0112, 'Anti-Depressants', 'Helps with depression', 15.99, 120, 'AbbVie Inc.', '2025', '2', '14');
+INSERT INTO product_item VALUES(8, 3648, 'Codeine Cough Syrup', 'Helps soothe a cough or sore throat', 8.49, 43, 'Hi-Tech Pharmaceuticals Inc.', '2024', '3', '20');
+INSERT INTO product_item VALUES(9, 7452, 'Medicinal Cannabis', 'Used to treat forms of nausea and epilepsy', 14.99, 420, 'Buds & Roses', '2024', '5', '9');
+INSERT INTO product_item VALUES(10, 1256, 'Viagra', 'Helps to achieve and maintain an erection', 149.99, 23, 'Pfizer', '2024', '12', '2');
--- Database_Supplier.sql
+++ Database_Supplier.sql
@@ -2,15 +2,18 @@ DROP DATABASE IF EXISTS sql_supplier;
 CREATE DATABASE sql_supplier;
 USE sql_supplier;
 
-CREATE TABLE supplier (
-	row_num INT (10) NOT NULL,
+CREATE TABLE supplier_info (
 	supplier_id INT (6) NOT NULL AUTO_INCREMENT,
+    row_num INT (10) NOT NULL,
 	supplier_name VARCHAR (50) NOT NULL,
 	supplier_email VARCHAR (50) NOT NULL,
 	supplier_phonenum VARCHAR(50) NOT NULL,
 	PRIMARY KEY (supplier_id));
 
-INSERT INTO supplier VALUES(1, 101, 'CVS', 'cvs@cvs.com', '909-420-6969');
-INSERT INTO supplier VALUES(2, 102, 'Johnson & Johnson', 'johnsonandjohnson@johnsonandjohnson.com', '820-450-6532');
-INSERT INTO supplier VALUES(3, 103, 'Pfizer', 'pfizer@pfizer.com', '450-642-8330');
-INSERT INTO supplier VALUES(4, 104, 'Buds & Roses', 'budsandroses@budsandroses.com', '420-420-4200');
+INSERT INTO supplier_info VALUES(1, 101, 'CVS', 'cvs@cvs.com', '909-420-6969');
+INSERT INTO supplier_info VALUES(2, 102, 'Johnson & Johnson', 'johnson&johnson@johnson&johnson.com', '820-450-6532');
+INSERT INTO supplier_info VALUES(3, 103, 'Pfizer', 'pfizer@pfizer.com', '450-642-8330');
+INSERT INTO supplier_info VALUES(4, 104, 'Buds & Roses', 'buds&roses@buds&roses.com', '420-420-4200');
+INSERT INTO supplier_info VALUES(5, 105, 'Endo International', 'endointernational@endointernational.com', '951-357-5623');
+INSERT INTO supplier_info VALUES(6, 106, 'AbbVie Inc.', 'abbvie@abbvie.com', '357-125-6547');
+INSERT INTO supplier_info VALUES(7, 107, 'Hi-Tech Pharmaceuticals Inc.', 'hitechpharmaceuticals@hitechpharmaceuticals.com', '659-856-4253');
